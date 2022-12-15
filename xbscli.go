21a13ca7534c582b5f02cd1288eade37a215e28e
マリウス <marius@xn--gckvb8fzb.com>
2022-12-15 00:01:54 -0500

    Implemented Bookmark struct, parsing

--- xbscli.go
+++ xbscli.go
@@ -17,6 +17,13 @@ import (
 	"golang.org/x/crypto/pbkdf2"
 )
 
+type Bookmark struct {
+	ID       int        `json:"id"`
+	Title    string     `json:"title"`
+	URL      string     `json:"url,omitempty"`
+	Children []Bookmark `json:"children,omitempty"`
+}
+
 type BookmarksResponse struct {
 	Bookmarks   string    `json:"bookmarks"`
 	LastUpdated time.Time `json:"lastUpdated"`
@@ -28,10 +35,20 @@ var syncID string
 var password string
 
 func main() {
-	flag.StringVar(&serverURL, "s", "", "the server URL")
-	flag.StringVar(&syncID, "i", "", "the sync ID")
-	flag.StringVar(&password, "p", "", "the sync password")
+	flag.StringVar(&serverURL, "s", "", "the server URL (required)")
+	flag.StringVar(&syncID, "i", "", "the sync ID (required)")
+	flag.StringVar(&password, "p", "", "the sync password (required)")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
 	flag.Parse()
+
+	if serverURL == "" || syncID == "" || password == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	response, err := http.Get(serverURL + "/bookmarks/" + syncID)
 	if err != nil {
@@ -55,7 +72,8 @@ func main() {
 
 	ciphertext, err := base64.StdEncoding.DecodeString(bookmarksResponse.Bookmarks)
 	if err != nil {
-		panic(err)
+		fmt.Print(err.Error())
+		os.Exit(1)
 	}
 
 	iv := ciphertext[:16]
@@ -63,18 +81,29 @@ func main() {
 
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err.Error())
+		fmt.Print(err.Error())
+		os.Exit(1)
 	}
 
 	aesgcm, err := cipher.NewGCMWithNonceSize(block, 16)
 	if err != nil {
-		panic(err)
+		fmt.Print(err.Error())
+		os.Exit(1)
 	}
 
 	encdata, err = aesgcm.Open(nil, iv, encdata, nil)
 	if err != nil {
-		panic(err)
+		fmt.Print(err.Error())
+		os.Exit(1)
 	}
 
-	fmt.Printf("%v\n", lz77.DecompressBlockToString(encdata))
+	bms := lz77.DecompressBlockToString(encdata)
+
+	var bookmarks []Bookmark
+	if err := json.Unmarshal([]byte(bms), &bookmarks); err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("%+v\n", bookmarks)
 }
